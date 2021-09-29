e855007f5acd4fffa5fe21e1b0d8314cf5da3eb9
truong <truongn661@gmail.com>
2021-09-29 10:46:49 +0700

    create heart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="heart"/>
+          <p style={{position: "absolute", top: "100%", right: 10, fontSize: "12px", color: "orange"}}>@developer: quangtruong</p>
+      </div>
   );
 }
 
